36f029bffb2ee5cb43e4028934d1ff2a344ca53c
sumeet-ht <sumeetp@heaptrace.com>
2021-05-04 17:46:42 +0530

    Spring Boot Project Setup

---
+++ src/main/java/com/inspekt/Inspekt/InspektApplication.java
@@ -0,0 +1,13 @@
+package com.inspekt.Inspekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inspekt/Inspekt/InspektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspekt.Inspekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
